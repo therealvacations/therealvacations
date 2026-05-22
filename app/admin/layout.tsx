501265e9b2ee64008cf293a17a02dc9e37e75afc
The Real Vacations <contact@therealvacations.com>
2026-05-22 03:59:56 -0400

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,9 +2,11 @@ import { Analytics } from '@vercel/analytics/next';
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
-
-      {children}
-      <Analytics />
-
+    <html lang="en">
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
   );
 }
