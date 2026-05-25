5121dd236662898e1b96a23c5a3e24d19a5a9b5e
The Real Vacations <contact@therealvacations.com>
2026-05-25 19:04:52 -0400

    Update page.tsx

--- app/admin/bookings/page.tsx
+++ app/admin/bookings/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
